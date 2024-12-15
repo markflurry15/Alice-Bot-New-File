74baafdae68f65e6fdc60fb2466867f7eca44e50
markflurry15 <markleedongitomeniola15@gmail.com>
2024-12-15 09:44:39 +0800

    Update index.js

--- index.js
+++ index.js
@@ -44,12 +44,6 @@ const axios = require('axios');
 
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-	const htmlFiles = ['clock.html', 'analog.html', 'crazy.html', 'index.html', 'randomQuote.html', 'randomVideo.html'];
-	const randomFile = htmlFiles[Math.floor(Math.random() * htmlFiles.length)];
-	res.sendFile(path.join(__dirname, 'temp', randomFile));
-});
-
 app.listen(port, () => {
 	console.log(`Server is running on http://localhost:${port}`);
 });
